5d7f22ad316119ad79feba53cf5e028707908e8b
rinvel7 <rinavelasquez03@gmail.com>
2023-09-18 00:55:46 -0400

    put

--- Modulo II/api-rests/src/controladores/instrutores.js
+++ Modulo II/api-rests/src/controladores/instrutores.js
@@ -37,10 +37,37 @@ const cadastrarInstrutor = (req, res) =>{
   }
   instrutores.push(instrutor);
     return res.status(201).json(instrutor);
+};
+
+const atualizarInstrutor = (req, res)=> {
+    const { id } = req.params;
+    const {nome, email, status} = req.body;
+
+    if(!nome){
+      return res.status(400).json({mensagem: 'O nome e obrigatorio'})
+    }
+    if(!email){
+      return res.status(400).json({mensagem: 'O email e obrigatorio'})
+    }
+
+    const instrutor = instrutores.find((instrutor) =>{
+        return instrutor.id === Number(id);
+    });
+
+    if(!instrutor){
+        return res.status(404).json({ mensagem: 'Instrutor nao Encontrado' });
+    }
+
+    instrutor.nome = nome;
+    instrutor.email = email;
+    instrutor.status = status;
+
+    return res.status(203).send();
 }
 
 module.exports = {
     listarInstrutores,
     obterInstrutor,
-    cadastrarInstrutor
+    cadastrarInstrutor,
+    atualizarInstrutor
 }
