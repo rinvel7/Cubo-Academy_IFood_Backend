1719c34a4a77387e9eafea5651afe09220cd7d0a
rinvel7 <rinavelasquez03@gmail.com>
2023-09-20 17:43:27 -0400

    promise-all

--- Modulo II/api-express-funcoesAssincronas/src/index.js
+++ Modulo II/api-express-funcoesAssincronas/src/index.js
@@ -5,8 +5,14 @@ const { getCityFromZipcode } = require('utils-playground')
 const app = express()
 
 app.get('/', async (req, res)=>{
-    const cidade = await getCityFromZipcode ('69090355')
-    res.send(`A cidade encontrada foi: ${cidade}`)
+    const cidade = getCityFromZipcode ('69090355')
+    const cidade2 = getCityFromZipcode ('44380000')
+
+    const promise = await Promise.all([cidade, cidade2]);
+    const [resposta1, resposta2] = promise
+        //console.log(cidade, cidade2);
+
+    res.send(`A cidade encontrada foi: ${resposta1} e ${resposta2}`);
 });
 
 app.listen(3000);
