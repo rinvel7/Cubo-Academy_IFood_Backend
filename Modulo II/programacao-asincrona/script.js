d55a7f24bb642adf49da0c469b7bff1a31c0b18f
rinvel7 <rinavelasquez03@gmail.com>
2023-09-19 21:35:44 -0400

    then & catch

---
+++ Modulo II/programacao-asincrona/script.js
@@ -0,0 +1,9 @@
+/* const { getCityFromZipcode } = require('utils-playground');
+
+getCityFromZipcode('69090355').then(cidade =>{
+    console.log(cidade);
+}).catch((erro)=>{
+    console.log(erro);
+}) */
+
+
