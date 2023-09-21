d70019e9f5c44cb43d3a4513d769d5006f05a648
rinvel7 <rinavelasquez03@gmail.com>
2023-09-21 10:52:23 -0400

    exercicio resolvido

--- Modulo II/api-express-funcoesAssincronas/src/index.js
+++ Modulo II/api-express-funcoesAssincronas/src/index.js
@@ -1,5 +1,10 @@
+//EXERCICIO RESOLVIDO
+//Continuando a implemetacion da API utilizando funcaoes asincronas
+//crie uma rota GET /pacote/:nomePacote que retorne a descripcao de um pacote do npm. 
+//Para obter a descrcao do pacote, utilize a funcao getPackageDescriptionNpm que existe no pacote utils-playground, passando o nome do pacote como argumento.
+
 const express = require('express');
-const { getCityFromZipcode } = require('utils-playground')
+const { getCityFromZipcode, getPackageDescriptionNpm } = require('utils-playground')
 
 
 const app = express()
@@ -14,5 +19,16 @@ app.get('/', async (req, res)=>{
 
     res.send(`A cidade encontrada foi: ${resposta1} e ${resposta2}`);
 });
+
+//EXERCICIO 1
+app.get('/pacote/:nomePacote', async (req, res) =>{
+    const { nomePacote } = req.params;
+
+    const descricaoPacote = await getPackageDescriptionNpm(nomePacote);
+
+    return res.send(descricaoPacote);
+});
+
+//navegador: localhost:3000/pacote/express
 
 app.listen(3000);
