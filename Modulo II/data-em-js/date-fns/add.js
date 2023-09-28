b29a6fb3e6614f3379027b22141b586202595adc
rinvel7 <rinavelasquez03@gmail.com>
2023-09-28 08:49:33 -0400

    date-fns

---
+++ Modulo II/data-em-js/date-fns/add.js
@@ -0,0 +1,5 @@
+/* const { add } = require('date-fns');
+
+const date = new Date(2018, 2, 13, 12);
+
+console.log(date); */
---
+++ Modulo II/data-em-js/date-fns/addBusinessDays.js
@@ -0,0 +1,6 @@
+
+const {addBusinessDays} = require('date-fns')
+
+console.log(addBusinessDays(new Date(2014, 8, 1), 10));//2014-09-15T04:00:00.000Z
+
+//O dias impresso e aquele dia depois ha adicionado os 10 dias
