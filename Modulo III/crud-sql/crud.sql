5371f8640b35cbcec6105ae4d9aa74b9fdeab2db
rinvel7 <rinavelasquez03@gmail.com>
2023-10-11 14:41:12 -0400

    aula finalizada

---
+++ Modulo III/crud-sql/crud.sql
@@ -0,0 +1,23 @@
+--CREATE
+
+--INSERT so um produto
+--insert into produtos (nome, preco, descricao, categoria) values('Caderno', NULL, 2000, 'Material escolar');
+
+--INSERT varios produto de uma so vez
+--insert into produtos 
+--(nome, descricao, preco, categoria)
+--values
+--('Calca Jeans', NULL, 12900, 'Roupas'),
+--('Bermuda', 'Bermuda londa petra', 12900, 'Roupas')
+
+
+--UPDATE
+--update produtos
+--set
+--nome = 'Caderno de 10 materias'
+--where id = 3;
+
+
+--DELETE
+--delete from produtos
+--where id = 3;
